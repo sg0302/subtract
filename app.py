9bb5621c5aa2d35e8e0acdfe92d893f893461b1c
sg0302 <119248637+sg0302@users.noreply.github.com>
2022-11-28 17:14:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+title = "subtract 2 numbers"
+st.set_page_config(page_title=title)
+
+st.title('subtract 2 numbers')
+
+x = st.number_input('Enter first number')
+y = st.number_input('Enter second number')
+z=x-y
+st.write(z,"is the largest number")
